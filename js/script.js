8305493e3ddde3134f7d53bfd8164cd3ab44a97c
sarahuyghe <sara.huyghe@student.howest.be>
2020-04-23 21:34:48 +0200

    added styling footer + nav

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 {
   const $topButton = document.querySelector("#toTop")
+  const $nav = document.querySelector('header')
+  const $logo = document.querySelector('#logo')
+
+
+  const scrollFunction = () => {
+    const y = window.scrollY;
+    if (y > 300) {
+      $nav.classList.add('bg-navbar')
+      $logo.classList.add('fadeIn')
+    } else {
+      $nav.classList.remove('bg-navbar')
+      $logo.classList.remove('fadeIn')
+
+    }
+  }
+
   const init = () => {
+    window.addEventListener(`scroll`, scrollFunction)
+
     $topButton.addEventListener(`click`, () => window.scrollTo({
       top: 0,
       behavior: 'smooth'
