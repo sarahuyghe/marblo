3fb1fabd8ae7bdf72910b26d4dcceb3b90a4acfb
sarahuyghe <sara.huyghe@student.howest.be>
2020-05-07 11:30:46 +0200

    added smooth scolling

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@
   const $topButton = document.querySelector("#toTop")
   const $nav = document.querySelector('.navbar')
   const $logo = document.querySelector('#logo')
-  const $carouselItems = document.querySelectorAll('#carousel-itemText')
-  const $carouselImages = document.querySelectorAll('.carousel-item')
-
-
+  const $navLinks = document.querySelectorAll('.nav-link')
 
   const scrollFunction = () => {
     const y = window.scrollY;
@@ -15,28 +12,19 @@
     } else {
       $nav.classList.remove('bg-navbar')
       $logo.classList.remove('fadeIn')
-
     }
   }
 
-  // const slideCarousel = e => {
-  //   $number = e.currentTarget.getAttribute(`data-slide-to`)
-  //   $carouselItems.forEach(item => item.classList.remove(`active`))
-  //   e.currentTarget.classList.add(`active`)
-  //   $carouselImages.forEach(item => {
-  //     item.classList.remove('active')
-  //     if (item.getAttribute(`data-slide-to`) === $number) {
-  //       item.classList.add('active')
-  //     }
-  //     item.getAttribute(`data-slide-to`)
-  //   })
-  // }
+  const scrollToFunction = (e) => {
+    e.preventDefault();
+    document.querySelector('#' + e.currentTarget.dataset.id + '').scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
 
   const init = () => {
-    window.addEventListener(`scroll`, scrollFunction)
-    $carouselItems.forEach(item => {
-      item.addEventListener(`click`, slideCarousel)
-    })
+    $navLinks.forEach(link => link.addEventListener(`click`, scrollToFunction))
+    window.addEventListener(`scroll`, scrollFunction);
     $topButton.addEventListener(`click`, () => window.scrollTo({
       top: 0,
       behavior: 'smooth'
